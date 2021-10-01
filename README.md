32a455ddc19d8c3f43317a42e295e75290ef350a
William T Olson <trevor@hiringsolved.com>
2021-10-01 13:49:58 -0700

    Use cibuildwheel

--- README.md
+++ README.md
@@ -2,9 +2,3 @@
 
 Scripts for building self contained python wheels of
 [PyICU](https://github.com/ovalhub/pyicu) for linux and macosx.
-
-    make
-    make release-test
-    make release
-
-**Note:** macosx builds must be built on a mac.
---
+++ scripts/prepare-linux.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+PYTHON_BIN=/opt/python/cp39-cp39/bin
+BUILD_DIR="${PWD}/build"
+
+# Download icu4c
+curl -L "https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/_/-}/icu4c-${ICU_VERSION}-src.tgz" \
+    > "${BUILD_DIR}/icu4c-${ICU_VERSION}-src.tgz"
+
+curl -L "https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/_/-}/icu4c-${ICU_VERSION}-src.tgz.asc" \
+    > "${BUILD_DIR}/icu4c-${ICU_VERSION}-src.tgz.asc"
+
+# Verify
+gpg --import /build/KEYS
+gpg --verify "${BUILD_DIR}/icu4c-${ICU_VERSION}-src.tgz.asc" "${BUILD_DIR}/icu4c-${ICU_VERSION}-src.tgz"
+
+# Unpack
+tar -C "${BUILD_DIR}/" -xzf "${BUILD_DIR}/icu4c-$ICU_VERSION-src.tgz"
+
+# Build and install icu4c
+(cd "${BUILD_DIR}/icu/source" && PATH=$PYTHON_BIN:$PATH ./runConfigureICU Linux)
+(cd "${BUILD_DIR}/icu/source" && make)
+(cd "${BUILD_DIR}/icu/source" && make install)
+
+# Download PyICU source
+$PYTHON_BIN/pip download \
+    --no-binary=:all: \
+    --no-deps \
+    --dest "${BUILD_DIR}" \
+    "PyICU==${PYICU_VERSION}"
+
+tar -C "${BUILD_DIR}/" -xmzf "${BUILD_DIR}/PyICU-$PYICU_VERSION.tar.gz"
+
+# Replace the package name
+patch --verbose -p1 -d "${BUILD_DIR}/PyICU-$PYICU_VERSION" < pyicu.patch
